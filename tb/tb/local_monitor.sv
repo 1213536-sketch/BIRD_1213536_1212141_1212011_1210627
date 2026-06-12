8d4a058852cc0532cb973c02c21f9a16b2eb9bb5
1213536-sketch <1213536@student.birzeit.edu>
2026-06-12 21:15:06 +0300

    Update local_monitor.sv

--- tb/tb/local_monitor.sv
+++ tb/tb/local_monitor.sv
@@ -1,3 +1,14 @@
 class local_monitor;
 
+  virtual bird_if vif;
+  mailbox #(bird_packet) mon2sb;
+
+  function new(
+    virtual bird_if vif,
+    mailbox #(bird_packet) mon2sb
+  );
+    this.vif    = vif;
+    this.mon2sb = mon2sb;
+  endfunction
+
 endclass
