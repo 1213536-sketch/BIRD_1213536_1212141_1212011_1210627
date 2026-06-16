447eb4dcf62c1749cc8c71a286667b21c4d4cdaa
Fatima <fatimazahran2003@gmail.com>
2026-06-16 22:13:36 +0300

    add packeges and brid packet

---
+++ tb/bird_pkg.sv
@@ -0,0 +1,16 @@
+package bird_pkg;
+
+  class bird_packet;
+    bit [7:0] payload[];
+    bit [7:0] crc[1:0];
+
+    bit [3:0] traffic_type;
+    bit [7:0] payload_len;
+    bit [4:0] frag_num;
+    bit [4:0] seq_num;
+
+    function new();
+    endfunction
+  endclass
+
+endpackage
