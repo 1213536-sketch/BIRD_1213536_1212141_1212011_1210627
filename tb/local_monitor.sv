35cc0ef9e7960acf7d82b404fdfa68fc1fc309ad
Fatima <fatimazahran2003@gmail.com>
2026-06-16 22:21:25 +0300

    update to match the compersion in scoreboard

--- tb/local_monitor.sv
+++ tb/local_monitor.sv
@@ -3,11 +3,8 @@ class local_monitor;
   virtual bird_if vif;
   mailbox #(bird_packet) mon2sb;
 
-  function new(
-    virtual bird_if vif,
-    mailbox #(bird_packet) mon2sb
-  );
-    this.vif    = vif;
+  function new(virtual bird_if vif, mailbox #(bird_packet) mon2sb);
+    this.vif = vif;
     this.mon2sb = mon2sb;
   endfunction
 
@@ -17,27 +14,27 @@ class local_monitor;
 
     forever begin
 
-  @(posedge vif.clk);
+      @(posedge vif.clk);
 
-  if (vif.local_vld && vif.local_rdy) begin
+      if (vif.local_vld && vif.local_rdy) begin
 
-    pkt = new();
+        pkt = new();
 
-    pkt.payload_len = 1;
-    pkt.payload = new[1];
+        pkt.payload_len = 1;
+        pkt.payload = new[1];
 
-    pkt.payload[0] = vif.data_local;
+        pkt.payload[0] = vif.data_local;
 
-    pkt.crc[0] = 8'h00;
-    pkt.crc[1] = 8'h00;
+        pkt.crc[0] = 0;
+        pkt.crc[1] = 0;
 
-    mon2sb.put(pkt);
+        mon2sb.put(pkt);
 
-    $display("[LOCAL MONITOR] Data Captured");
+        $display("[LOCAL MON] Data captured");
 
-  end
+      end
 
-end
+    end
 
   endtask
 
